759b32a8824645e83897354ea90f44ecc3a55188
Peter Lam <Peter.Lam@tufts.edu>
2020-01-30 12:25:26 -0500

    Updates

---
+++ precache-manifest.4f4d5d754873d9fcfec2551f7daa046b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28facfffa88256ddc56adf1a446fd1a",
+    "url": "/doom-clock/index.html"
+  },
+  {
+    "revision": "63a7267d073b85889ae9",
+    "url": "/doom-clock/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "8841c30dd2d064c7d951",
+    "url": "/doom-clock/static/css/main.d8adf44d.chunk.css"
+  },
+  {
+    "revision": "63a7267d073b85889ae9",
+    "url": "/doom-clock/static/js/2.d8b16d9d.chunk.js"
+  },
+  {
+    "revision": "14cfafeab58f243640d9d112b6ecad16",
+    "url": "/doom-clock/static/js/2.d8b16d9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8841c30dd2d064c7d951",
+    "url": "/doom-clock/static/js/main.88a18c8b.chunk.js"
+  },
+  {
+    "revision": "508e589341c95737500a",
+    "url": "/doom-clock/static/js/runtime-main.278621df.js"
+  },
+  {
+    "revision": "b6a60dc0474c85793187229ba13d1a04",
+    "url": "/doom-clock/static/media/racing-flag.b6a60dc0.png"
+  },
+  {
+    "revision": "8d6b7968dd055e79d776f19370a0c100",
+    "url": "/doom-clock/static/media/start-up.8d6b7968.png"
+  },
+  {
+    "revision": "95d798db49f47371b6b6aea10ec07e03",
+    "url": "/doom-clock/static/media/typewriter.95d798db.png"
+  }
+]);
